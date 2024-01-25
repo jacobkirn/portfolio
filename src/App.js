1dc39528b4b73d86a7f25777377e7c3a4e078ca9
jacobkirn <156028317+jacobkirn@users.noreply.github.com>
2024-01-25 04:29:25 +0000

    testing

--- src/App.js
+++ src/App.js
@@ -9,52 +9,52 @@ import StyleCo from './StyleCo';
 import Footer from './components/Footer';
 
 function App() {
-  return (
-    <div>
-      <Router>
-        <div id='d'>
-          <AnimatedCursor
-            innerSize={20}
-            outerSize={40}
-            innerScale={1}
-            outerScale={1.5}
-            outerAlpha={0.2}
-            showSystemCursor={false}
-            hasBlendMode={true}
-            innerStyle={{
-              backgroundColor: '#f9f9f9',
-              mixBlendMode: 'exclusion'
-            }}
-            outerStyle={{
-              border: '4px solid #f9f9f9',
-              mixBlendMode: 'exclusion'
-            }}
-            clickables={[
-              'a',
-              'input[type="text"]',
-              'input[type="email"]',
-              'input[type="number"]',
-              'input[type="submit"]',
-              'input[type="image"]',
-              'label[for]',
-              'select',
-              'textarea',
-              'button',
-              '#folders',
-              '.popup-header'
-            ]}
-          />
-        </div>
-        <NavBar />
-        < Routes >
-          <Route path="/portfolio/styleco" element={<StyleCo />} />
-          <Route path="/portfolio" element={<Portfolio />} />
-          <Route path="/" element={<Home />} />
-        </Routes>
-        <Footer />
-      </Router>
-    </div >
-  );
+	return (
+		<div>
+			<Router>
+				<div id='d'>
+					<AnimatedCursor
+						innerSize={20} S
+						outerSize={40}
+						innerScale={1}
+						outerScale={1.5}
+						outerAlpha={0.2}
+						showSystemCursor={false}
+						hasBlendMode={true}
+						innerStyle={{
+							backgroundColor: '#f9f9f9',
+							mixBlendMode: 'exclusion'
+						}}
+						outerStyle={{
+							border: '4px solid #f9f9f9',
+							mixBlendMode: 'exclusion'
+						}}
+						clickables={[
+							'a',
+							'input[type="text"]',
+							'input[type="email"]',
+							'input[type="number"]',
+							'input[type="submit"]',
+							'input[type="image"]',
+							'label[for]',
+							'select',
+							'textarea',
+							'button',
+							'#folders',
+							'.popup-header'
+						]}
+					/>
+				</div>
+				<NavBar />
+				< Routes >
+					<Route path="/portfolio/styleco" element={<StyleCo />} />
+					<Route path="/portfolio" element={<Portfolio />} />
+					<Route path="/" element={<Home />} />
+				</Routes>
+				<Footer />
+			</Router>
+		</div >
+	);
 }
 
 export default App;
--- src/StyleCo.js
+++ src/StyleCo.js
@@ -1,15 +1,21 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import StyleCoHero from './images/deviceframes-2.png'
 
 function StyleCo() {
-  return <div>
-    <div>
-        <div className='container'>
-            <img src={StyleCoHero} alt="A collection of images from the StyleCo prototype"/>
-            <h1>Innovating Fashion Retail: Designing StyleCo's AR Fitting Room Experience</h1>
-        </div>
-    </div>
-  </div>;
+	return <div>
+		<div>
+			<div className='styleco'>
+				<div className='container'>
+					<p><Link to="/" id="breadcrumb-item">Home</Link> / <Link to="/portfolio" id="breadcrumb-item">Portfolio</Link></p>
+					<h1>Designing StyleCo's AR Fitting Room</h1>
+					<p id="mb-20">🎓 Class Project &nbsp;&nbsp;&nbsp;⏳ 5 minute read</p>
+
+					<img src={StyleCoHero} alt="A collection of images from the StyleCo prototype" width={"100%"} />
+				</div>
+			</div>
+		</div>
+	</div>;
 }
 
 export default StyleCo;
